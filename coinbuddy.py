6a1a948f9e0cae2d20b8d844d8736ffbe33ad417
Carter Hinsley <carterhinsley@outlook.com>
2017-05-22 15:54:37 -0400

    Add newlines for better readability

--- coinbuddy.py
+++ coinbuddy.py
@@ -11,16 +11,20 @@ client = Client(apikeys.api_key, apikeys.api_secret)
 
 def get_account(client=client, account_id=apikeys.account_id):
     '''Get description of wallet account.'''
+
     return client.get_account(account_id)
 
 def get_price(client=client, currency_pair='ETH-USD', price_type='spot'):
     '''Get price of Ethereum or other currency.
     `price_type` may be 'spot', 'buy', or 'sell'.
     Returns an APIObject with two fields: `amount`, `currency`.'''
+
     response = client._get('v2', 'prices', currency_pair, price_type)
+
     return client._make_api_object(response, APIObject)
 
 def get_price_string(client=client, currency_pair='ETH-USD', price_type='spot'):
+
     '''Get a string detailing current time in UTC, currency pair requested,
     price, and currency value.
     `price_type` may be 'spot', 'buy', or 'sell'.
@@ -30,6 +34,7 @@ def get_price_string(client=client, currency_pair='ETH-USD', price_type='spot'):
         currency_pair=currency_pair,
         price_type='spot'
     )
+
     return '[%s] %s: %s %s' % (
         str(datetime.datetime.utcnow()).split('.')[0],
         currency_pair,
